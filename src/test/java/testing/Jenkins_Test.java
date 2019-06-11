fa1d880a37facf4aa1de017d02347b4d00fa38f9
ChetnaGupta <ChetnaGupta@9.85.89.91>
2019-06-11 11:47:06 +0530

    GitHub link

---
+++ src/test/java/testing/Jenkins_Test.java
@@ -0,0 +1,36 @@
+package testing;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class Jenkins_Test {
+	
+	WebDriver driver;
+	
+  @Test
+  public void LoginTest() {
+	  
+	  driver.findElement(By.name("password")).sendKeys("manzoor");
+	  driver.findElement(By.name("password")).sendKeys("manzoor");
+	  driver.findElement(By.name("login")).click();
+	  System.out.println("title after login is "+ driver.getTitle());
+  }
+  
+  @BeforeTest
+  public void beforeTest() {
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\ChetnaGupta\\Desktop\\Selenium files\\chromedriver_win32\\chromedriver.exe");
+	  driver= new ChromeDriver();
+	  driver.get("http://www.newtours.demoaut.com");
+	  System.out.println("current title is "+ driver.getTitle());
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
+
+}
